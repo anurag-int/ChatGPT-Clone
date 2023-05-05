d2a5e9461237260932ccefd4ac98afcc14f8d1df
anurag-int <ag6526803@gmail.com>
2023-05-06 00:23:00 +0530

    started front end

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className = "App">
+
+      <aside className = "sidemenu">
+        <h1>Aside</h1>
+      </aside>
+
+      <section className = "chatbox">
+        <h1>Section</h1>
+      </section>
+    </div>
+  );
+}
+
+export default App;
