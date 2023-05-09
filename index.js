c92de95189a23fdff5e85f2bff8c7a50faffba3c
anurag-int <ag6526803@gmail.com>
2023-05-10 02:27:35 +0530

    backend integrated

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// 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
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+    organization: "org-bmJdSn56sWOw1SjcWSAMbekY",
+    apiKey:
+    "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",
+});
+const openai = new OpenAIApi(configuration);
+const app = express();
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors());
+const port = 3080;
+app.use(express.urlencoded({extended : true}));
+
+app.post('/', async(req, res)=>{
+    const {message} = req.body;
+    
+    const response = await openai.createCompletion({
+        model : "text-davinci-003",
+        prompt : `${message}`,
+        max_tokens : 100,
+        temperature: 0.5,
+    });
+    // console.log(response.data.choices[0].text);
+    res.json({
+        // data: response.data
+        message: response.data.choices[0].text
+    })
+})
+
+app.listen(port, ()=>{
+    console.log(`Server listening at port ${port}`);
+})
